84fe84ec81ccd05b815d511706c5a4143e026c59
Joe_Panetta <jlpanetta1681@icloud.com>
2020-11-03 03:02:50 -0500

    getting errors i dont iunderstand going to get some help tomorrow

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
-const express = require("express");
+const express = require('express');
 
-const db = require("../data/dbConfig.js");
-
+const db = require('../data/dbConfig.js');
+const accountsRouter = require('../accounts/accounts-router.js');
 const server = express();
 
 server.use(express.json());
+
+server.use('/data/dbConfig.js');
+
+server.use('/api/accounts', accountsRouter);
 
 module.exports = server;
